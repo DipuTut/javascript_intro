4285d9ac34a25c43257901c12abfc44a33a5bf70
DipuTut <mddipu@gmail.com>
2024-03-18 13:09:06 +0100

    Adding the elections website with conditions.

---
+++ conditions.js
@@ -0,0 +1,10 @@
+let uhrzeit = 22;
+
+if (uhrzeit < 18) {
+    console.log("Guten Tag");    
+} else if (uhrzeit < 22){
+    console.log("Guten Abend");
+} else {
+    console.log("Gute Nacht")
+}
+ 
